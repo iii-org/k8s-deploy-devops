b6f9c091612bc091a8bb58cd42b824a8b71d2263
ziyi-bear <54978757+ziyi-bear@users.noreply.github.com>
2021-01-27 10:09:46 +0800

    Update install-helm-repo.sh

--- install-helm-repo.sh
+++ install-helm-repo.sh
@@ -3,3 +3,5 @@ snap install helm --classic
 helm repo add jetstack https://charts.jetstack.io
 # rancher repo
 helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
+# gitlab
+helm repo add gitlab https://charts.gitlab.io/
