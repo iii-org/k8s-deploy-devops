d68154777f58bb1fd9e6a7ae1ca04e64a9c3fb59
ziyi-bear <m0724001@gm.nuu.edu.tw>
2021-01-27 07:44:17 +0000

    add install gitlab success.yaml

--- install-devops.sh
+++ install-devops.sh
@@ -4,3 +4,9 @@ helm install -n devops devops-redis bitnami/redis -f ./share-database/devops-sha
 
 # 等待安裝完畢 因為後面都必須仰賴連上上面這些資料庫
 # 設定資料庫->建立必須的資料表
+
+# 安裝gitlab
+kubectl apply -f ./gitlab-init-root-password.yaml
+helm install -n devops devops-gitlab gitlab/gitlab -f ./devops-gitlab-lite.yaml --timeout 3600s
+
+#
