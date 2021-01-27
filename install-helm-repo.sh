f1d6c3e8001d4c56618f57bf2f583404ab89636b
ziyi-bear <54978757+ziyi-bear@users.noreply.github.com>
2021-01-27 10:10:37 +0800

    Update install-helm-repo.sh

--- install-helm-repo.sh
+++ install-helm-repo.sh
@@ -5,3 +5,9 @@ helm repo add jetstack https://charts.jetstack.io
 helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
 # gitlab
 helm repo add gitlab https://charts.gitlab.io/
+# bitnami(這裡使用redmine)
+helm repo add bitnami https://charts.bitnami.com/bitnami
+# harbor
+helm repo add harbor https://helm.goharbor.io
+# adminer
+helm repo add cetic https://cetic.github.io/helm-charts
