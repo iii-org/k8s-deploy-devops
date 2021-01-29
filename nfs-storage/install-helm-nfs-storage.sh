7878285ec6cc575288a87f8334e0a19748ecfda3
ziyi-bear <54978757+ziyi-bear@users.noreply.github.com>
2021-01-29 09:13:32 +0800

    Update install-helm-nfs-storage.sh

--- nfs-storage/install-helm-nfs-storage.sh
+++ nfs-storage/install-helm-nfs-storage.sh
@@ -1,3 +1,6 @@
 helm repo add ckotzbauer https://ckotzbauer.github.io/helm-charts
 
 helm install iiidevops-nfs -n devops -f ./helm-iiidevops-nfs-storage.yaml ckotzbauer/nfs-client-provisioner
+
+## (注意)每個Node都要安裝
+sudo apt install nfs-common
